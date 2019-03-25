d794b055ba073173f1e4fbe4685b53146f66fd91
Arm Suwansiri <ysuwansiri@yahoo.com>
2019-03-25 18:14:29 +0700

    test left right direction with fuyu stepper motor

---
+++ main.js
@@ -0,0 +1,89 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/cu.usbmodem1421', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+let mainWindow;
+
+// Read the port data
+port.on("open", () => {
+    console.log('serial port open');
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow();
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const _writeMessage = (message) => {
+    port.write(message, (err) => {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+        console.log('message written');
+    });
+}
+
+// Catch button:on
+ipcMain.on('button:on', (e, item) => {
+    _writeMessage(item);
+    console.log(item);
+});
+
+// Catch button:off
+ipcMain.on('button:off', (e, item) => {
+    _writeMessage(item);
+    console.log(item);
+});
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label: "File",
+        submenu: [
+            {
+                label: 'Add Item'
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit'
+            }
+        ]
+    }
+];
+
+// Add dev tools item
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
