1487f01bfc4b5ac598143273a3d20590045e69cb
Arm Suwansiri <ysuwansiri@yahoo.com>
2019-04-27 11:36:26 +0700

    added release build

---
+++ release-builds/button-test-darwin-x64/button-test.app/Contents/Resources/app/main.js
@@ -0,0 +1,100 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/cu.usbmodem1411', { baudRate: 9600, 
+    dataBits: 8, 
+    parity: 'none', 
+    stopBits: 1, 
+    flowControl: false  });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+let mainWindow;
+
+// Read the port data
+port.on("open", () => {
+    console.log('serial port open');
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow();
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const _writeMessage = (message) => {
+    port.write(message, (err) => {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+        console.log('message written');
+    });
+}
+
+// Catch button:on
+ipcMain.on('button:on', (e, item) => {
+    _writeMessage(item);
+    console.log(item);
+});
+
+// Catch button:off
+ipcMain.on('button:off', (e, item) => {
+    _writeMessage(item);
+    console.log(item);
+});
+
+// Catch swipe left or right
+ipcMain.on('swipe:left-right', (e, direction, pixel) => {
+    const message = direction + ',' + pixel + '\n';
+    _writeMessage(message);
+    console.log(message);
+});
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label: "File",
+        submenu: [
+            {
+                label: 'Add Item'
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit'
+            }
+        ]
+    }
+];
+
+// Add dev tools item
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
