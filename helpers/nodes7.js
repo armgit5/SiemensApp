ed11739e0b783a514794c511f64f75df3e5fdf34
Arm Suwansiri <ysuwansiri@yahoo.com>
2019-07-16 00:02:52 +0700

    organize node in node class to handle many connections

--- helpers/nodes7.js
+++ helpers/nodes7.js
@@ -1,60 +1,80 @@
 const { ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const nodes7 = require('nodes7');  // This is the package name, if the repository is cloned you may need to require 'nodeS7' with uppercase S
 
-module.exports = () => {
-    
-    const nodes7 = require('nodes7');  // This is the package name, if the repository is cloned you may need to require 'nodeS7' with uppercase S
-    const conn = new nodes7;
-    let doneReading = false;
-    let doneWriting = false;
+class Node {
+    constructor(id, ip) {
+        this.id = id;
+        this.ip = ip;
+        this.doneReading = false;
+        this.doneWriting = false;
+        this.conn = new nodes7;
+        this.initConnection();
+        this.variables = {
+           TEST1: 'M4.0'
+        };
+    }
 
-    var variables = {
-        TEST1: 'MR4', 		// Memory real at MD4
-        TEST2: 'MW10', 		// Bit at M32.2
-        TEST3: 'M4.0', 		// Bit at M20.0
-        TEST4: 'DB1,DATETIME',	// Array of 20 values in DB1
-        TEST5: 'DB1,REAL4',		// Single real value
-        TEST6: 'DB1,REAL8',		// Another single real value
-        TEST7: 'DB1,INT12.2'		// Two integer value array
-    };
+    initConnection() {
+        this.conn.initiateConnection({ port: 102, host: this.ip, rack: 0, slot: 1 }, this.connected); // slot 2 for 300/400, slot 1 for 1200/1500
+    }
 
-    conn.initiateConnection({ port: 102, host: '192.168.0.11', rack: 0, slot: 1 }, connected); // slot 2 for 300/400, slot 1 for 1200/1500
-    
-    function connected(err) {
+    connected(err) {
         if (typeof (err) !== "undefined") {
             // We have an error.  Maybe the PLC is not reachable.
             console.log(err);
-            // process.exit();
+        } else {
+            this.conn.setTranslationCB((tag) => { return this.variables[tag]; }); 	// This sets the "translation" to allow us to work with object names
         }
-
-        conn.setTranslationCB(function (tag) { return variables[tag]; }); 	// This sets the "translation" to allow us to work with object names
     }
 
-    function valuesReady(anythingBad, values) {
+    valuesReady(anythingBad, values) {
         if (anythingBad) { console.log("SOMETHING WENT WRONG READING VALUES!!!!"); }
         console.log(values);
-        doneReading = true;
-        if (doneWriting) { process.exit(); }
+        this.doneReading = true;
+        if (this.doneWriting) { process.exit(); }
     }
 
-    function valuesWritten(anythingBad) {
+    valuesWritten(anythingBad) {
         if (anythingBad) { console.log("SOMETHING WENT WRONG WRITING VALUES!!!!"); }
         console.log("Done writing.");
-        doneWriting = true;
-        if (doneReading) { process.exit(); }
+        this.doneWriting = true;
+        if (this.doneReading) { process.exit(); }
     }
 
+}
+
+module.exports = () => {
+    const connections = {}; // Hold nodes connection
+
+    // Read json file
+    const p = path.join(
+        path.dirname(process.mainModule.filename),
+        'data',
+        'stations.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            console.log('error reading file');
+        }
+        const data = JSON.parse(fileContent);
+        console.log(data);
+        const node = new Node(data[0].id, data[0].ip);
+        connections[data[0].id] = node;
+    });
+   
     // IPC calls
     // Catch item:add
-    ipcMain.on('m4:click', (e, status) => {
-        console.log(status);
-        conn.writeItems('TEST3', true, valuesWritten);
+    ipcMain.on('m4:click', (e, id, status) => {
+        console.log(id, status);
+        const node = connections[id];
+        node.conn.writeItems('M4.0', true, node.valuesWritten);
     });
 
-    ipcMain.on('m4:clickoff', (e, status) => {
+    ipcMain.on('m4:clickoff', (e, id, status) => {
         console.log(status);
-        conn.writeItems('TEST3', false, valuesWritten);
+        const node = connections[id];
+        node.conn.writeItems('M4.0', false, node.valuesWritten);
     });
-
-    // ----------   Node 7  -------------
-
 }
