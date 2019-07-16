e9f893ce8dbea26ff3de678b076a29e3a55aa16c
Yuttanant Suwansiri <ysuwansiri@yahoo.com>
2019-07-16 23:01:04 +0700

    tested m4 read write

--- helpers/nodes7.js
+++ helpers/nodes7.js
@@ -31,10 +31,22 @@ module.exports = (mainWindow) => {
         const node = connections[id];
         node.conn.addItems('M4.0');
         node.conn.readAllItems(node.valuesReady);
-        node.conn.writeItems('M4.0', true, node.valuesWritten);
+        node.conn.writeItems('M6.0', true, node.valuesWritten);
+
+        // Read M Items
         node.conn.addItems('M4.0');
+        node.conn.addItems('M4.0');
+        node.conn.addItems('MW80');
+        node.conn.addItems('MW82');
+
+        // Read Data Blocks
+        node.conn.addItems('DB8,INT6.4');
+
         node.conn.readAllItems(node.valuesReady);
         mainWindow.webContents.send('get:connections', connections);
+
+        // Assign read to M4
+        const M4 = node.conn.readAllItems(node.valuesReady);
     });
 
     ipcMain.on('m4:clickoff', (e, id, status) => {
@@ -42,8 +54,10 @@ module.exports = (mainWindow) => {
         const node = connections[id];
         node.conn.addItems('M4.0');
         node.conn.readAllItems(node.valuesReady);
-        node.conn.writeItems('M4.0', false, node.valuesWritten);
+        node.conn.writeItems('M6.0', false, node.valuesWritten);
         node.conn.addItems('M4.0');
         node.conn.readAllItems(node.valuesReady);
+
+
     });
 }
