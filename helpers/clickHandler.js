8a01757e9d47949840fa664f79d726dda21496c5
Arm Suwansiri <ysuwansiri@yahoo.com>
2019-07-20 10:19:16 +0700

    add click handler

--- helpers/clickHandler.js
+++ helpers/clickHandler.js
@@ -1,41 +1,75 @@
 const { ipcMain } = require('electron');
+const { CHANNELS, SCANTIME } = require('./environments');
+const STATIONS = require('../data/stations');
 
-module.exports = {
+const Store = require('electron-store');
+const store = new Store();
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+module.exports = (connections) => {
+
+    // Button Click
+    ipcMain.on(CHANNELS.ll1On, (e, status) => {
+        const node = connections[STATIONS[0].id];
+        const dateTime = STATIONS[0].dateTime;
+        const step1 = dateTime.step1;
+
+        node.conn.writeItems(STATIONS[0].bits.ll1On, true, node.valuesWritten);
+        setTimeout(() => {
+            node.conn.writeItems(STATIONS[0].bits.ll1On, false, node.valuesWritten);
+        }, 1000);
+
+
+        node.conn.writeItems(step1.setOnHH, 23, node.valuesWritten);
+        node.conn.readAllItems(node.valuesReady);
+
+    });
+
+    ipcMain.on(CHANNELS.ll1Off, (e, status) => {
+        console.log(status);
+        const node = connections[STATIONS[0].id];
+        node.conn.writeItems(STATIONS[0].bits.ll1Off, true, node.valuesWritten);
+        setTimeout(() => {
+            node.conn.writeItems(STATIONS[0].bits.ll1Off, false, node.valuesWritten);
+        }, 1000);
+    });
+
+    ipcMain.on(CHANNELS.ll2On, (e, status) => {
+        const node = connections[STATIONS[0].id];
+        node.conn.writeItems(STATIONS[0].bits.ll2On, true, node.valuesWritten);
+        setTimeout(() => {
+            node.conn.writeItems(STATIONS[0].bits.ll2On, false, node.valuesWritten);
+        }, 1000);
+    });
+
+    ipcMain.on(CHANNELS.ll2Off, (e, status) => {
+        console.log(status);
+        const node = connections[STATIONS[0].id];
+        node.conn.writeItems(STATIONS[0].bits.ll2Off, true, node.valuesWritten);
+        setTimeout(() => {
+            node.conn.writeItems(STATIONS[0].bits.ll2Off, false, node.valuesWritten);
+        }, 1000);
+    });
     
-    // IPC calls
-    // Catch item:add
-    // ipcMain.on('m4:click', (e, id, status) => {
-    //     console.log(id, status);
-    //     const node = connections[id];
-    //     node.conn.addItems('M4.0');
-    //     node.conn.readAllItems(node.valuesReady);
-    //     node.conn.writeItems('M6.0', true, node.valuesWritten);
+    ipcMain.on(CHANNELS.ll3On, (e, status) => {
+        const node = connections[STATIONS[0].id];
+        node.conn.writeItems(STATIONS[0].bits.ll3On, true, node.valuesWritten);
+        setTimeout(() => {
+            node.conn.writeItems(STATIONS[0].bits.ll3On, false, node.valuesWritten);
+        }, 1000);
+    });
 
-    //     // Read M Items
-    //     node.conn.addItems('M4.0');
-    //     node.conn.addItems('M4.0');
-    //     node.conn.addItems('MW80');
-    //     node.conn.addItems('MW82');
+    ipcMain.on(CHANNELS.ll3Off, (e, status) => {
+        console.log(status);
+        const node = connections[STATIONS[0].id];
+        node.conn.writeItems(STATIONS[0].bits.ll3Off, true, node.valuesWritten);
+        setTimeout(() => {
+            node.conn.writeItems(STATIONS[0].bits.ll3Off, false, node.valuesWritten);
+        }, 1000);
+    });
 
-    //     // Read Data Blocks
-    //     node.conn.addItems('DB8,INT6.4');
-
-    //     node.conn.readAllItems(node.valuesReady);
-    //     mainWindow.webContents.send('get:connections', connections);
-
-    //     // Assign read to M4
-    //     const M4 = node.conn.readAllItems(node.valuesReady);
-    // });
-
-    // ipcMain.on('m4:clickoff', (e, id, status) => {
-    //     console.log(status);
-    //     const node = connections[id];
-    //     node.conn.addItems('M4.0');
-    //     node.conn.readAllItems(node.valuesReady);
-    //     node.conn.writeItems('M6.0', false, node.valuesWritten);
-    //     node.conn.addItems('M4.0');
-    //     node.conn.readAllItems(node.valuesReady);
-
-
-    // });
+   
 }
--- helpers/nodes7.js
+++ helpers/nodes7.js
@@ -114,67 +114,6 @@ module.exports = (mainWindow) => {
     };
     _initNodes();
 
-
-
-    // Button Click
-    ipcMain.on(CHANNELS.ll1On, (e, status) => {
-        const node = connections[STATIONS[0].id];
-        const dateTime = STATIONS[0].dateTime;
-        const step1 = dateTime.step1;
-
-        node.conn.writeItems(STATIONS[0].bits.ll1On, true, node.valuesWritten);
-        setTimeout(() => {
-            node.conn.writeItems(STATIONS[0].bits.ll1On, false, node.valuesWritten);
-        }, 1000);
-
-
-        node.conn.writeItems(step1.setOnHH, 23, node.valuesWritten);
-        node.conn.readAllItems(node.valuesReady);
-
-    });
-
-    ipcMain.on(CHANNELS.ll1Off, (e, status) => {
-        console.log(status);
-        const node = connections[STATIONS[0].id];
-        node.conn.writeItems(STATIONS[0].bits.ll1Off, true, node.valuesWritten);
-        setTimeout(() => {
-            node.conn.writeItems(STATIONS[0].bits.ll1Off, false, node.valuesWritten);
-        }, 1000);
-    });
-
-    ipcMain.on(CHANNELS.ll2On, (e, status) => {
-        const node = connections[STATIONS[0].id];
-        node.conn.writeItems(STATIONS[0].bits.ll2On, true, node.valuesWritten);
-        setTimeout(() => {
-            node.conn.writeItems(STATIONS[0].bits.ll2On, false, node.valuesWritten);
-        }, 1000);
-    });
-
-    ipcMain.on(CHANNELS.ll2Off, (e, status) => {
-        console.log(status);
-        const node = connections[STATIONS[0].id];
-        node.conn.writeItems(STATIONS[0].bits.ll2Off, true, node.valuesWritten);
-        setTimeout(() => {
-            node.conn.writeItems(STATIONS[0].bits.ll2Off, false, node.valuesWritten);
-        }, 1000);
-    });
-    
-    ipcMain.on(CHANNELS.ll3On, (e, status) => {
-        const node = connections[STATIONS[0].id];
-        node.conn.writeItems(STATIONS[0].bits.ll3On, true, node.valuesWritten);
-        setTimeout(() => {
-            node.conn.writeItems(STATIONS[0].bits.ll3On, false, node.valuesWritten);
-        }, 1000);
-    });
-
-    ipcMain.on(CHANNELS.ll3Off, (e, status) => {
-        console.log(status);
-        const node = connections[STATIONS[0].id];
-        node.conn.writeItems(STATIONS[0].bits.ll3Off, true, node.valuesWritten);
-        setTimeout(() => {
-            node.conn.writeItems(STATIONS[0].bits.ll3Off, false, node.valuesWritten);
-        }, 1000);
-    });
-
-
+    // Receive click callls
+    require('./clickHandler')(connections);
 };
