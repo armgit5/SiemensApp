460ed025d6b5a3e7ff5419d8abed42f15e17a758
Arm Suwansiri <ysuwansiri@yahoo.com>
2019-07-22 14:04:13 +0700

    cleaned up main window page

--- helpers/nodes7.js
+++ helpers/nodes7.js
@@ -3,16 +3,12 @@ const Node = require('./node');
 const { CHANNELS, SCANTIME } = require('./environments');
 const STATION1 = require('../data/station1');
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-];
-
 const NODE; // Hold node connection
 let STATION_ID = 0;
 
 module.exports = (mainWindow) => {
 
-    const _addDatetimeReadList = () => {
+    const _addStation1DatetimeReadList = () => { // Will be used on every page
         const dateTime = STATION1.dateTime;
         const dateTimeHeader = dateTime.header;
 
@@ -23,15 +19,15 @@ module.exports = (mainWindow) => {
         NODE.conn.addItems(dateTimeHeader.hour);
     }
 
-
     const _initNode = () => {
         INTERVALS.forEach(clearInterval); // Clear interval
-        NODE = null; // Clear node
+        NODE = null; // Clear node to kill old connection
 
         // Init station 1
         if (STATION_ID === 1) {
             NODE = new Node(STATION1.id, STATION1.ip);
-            _addDatetimeReadList();
+            _addStation1DatetimeReadList();
+            require('./station1-process/readingHandler')(NODE, mainWindow);
         }
     };
 
@@ -43,15 +39,10 @@ module.exports = (mainWindow) => {
             // new connection to new plc
             if (id !== STATION_ID) {
                 STATION_ID = id;
-                console.log('Kill old process');
-                console.log('Start new process');
                 _initNode();
             }
         });
     }
 
     main();
-
-    // Receive click callls
-    // require('./clickHandler')(CONNECTION, mainWindow);
 };
