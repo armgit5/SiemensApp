8e55143bac3ada0d6090e5b589a07deccf18a63e
Arm Suwansiri <ysuwansiri@yahoo.com>
2019-07-24 10:15:48 +0700

    cleaned up ll on/off

--- helpers/station1-process/clickHandler.js
+++ helpers/station1-process/clickHandler.js
@@ -34,79 +34,65 @@ module.exports = (NODE, mainWindow) => {
     ipcMain.on(CHANNELS.ll1Off, (e, status) => {
         writeHelper(NODE, STATION1.bits.ll1Off, true)
             .then(_ => {
-                return writeHelper(NODE, STATION1.bits.ll1On, false);
+                return writeHelper(NODE, STATION1.bits.ll1Off, false);
             })
             .then(_ => {
             })
             .catch(err => {
                 console.log(err);
             });
-        // NODE.conn.writeItems(STATION1.bits.ll1Off, true, (anythingBad) => {
-        //     this.doneReading = true;
-        //     NODE.conn.writeItems(STATION1.bits.ll1Off, false,  (anythingBad) => {
-        //         this.doneReading = true;
-        //     });
-        // });
     });
 
     // ll3
     ipcMain.on(CHANNELS.ll2On, (e, status) => {
-        const NODE = NODE[STATION1.id];
-        NODE.conn.writeItems(STATION1.bits.ll2On, true, NODE.valuesWritten);
-        setTimeout(() => {
-            NODE.conn.writeItems(STATION1.bits.ll2On, false, NODE.valuesWritten);
-        }, 1000);
+        writeHelper(NODE, STATION1.bits.ll2On, true)
+            .then(_ => {
+                return writeHelper(NODE, STATION1.bits.ll2On, false);
+            })
+            .then(_ => {
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
 
     // ll4
     ipcMain.on(CHANNELS.ll2Off, (e, status) => {
-        console.log(status);
-        const NODE = NODE[STATION1.id];
-        NODE.conn.writeItems(STATION1.bits.ll2Off, true, NODE.valuesWritten);
-        setTimeout(() => {
-            NODE.conn.writeItems(STATION1.bits.ll2Off, false, NODE.valuesWritten);
-        }, 1000);
+        writeHelper(NODE, STATION1.bits.ll2Off, true)
+            .then(_ => {
+                return writeHelper(NODE, STATION1.bits.ll2Off, false);
+            })
+            .then(_ => {
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
     
     // ll5
     ipcMain.on(CHANNELS.ll3On, (e, status) => {
-        const NODE = NODE[STATION1.id];
-        NODE.conn.writeItems(STATION1.bits.ll3On, true, NODE.valuesWritten);
-        setTimeout(() => {
-            NODE.conn.writeItems(STATION1.bits.ll3On, false, NODE.valuesWritten);
-        }, 1000);
+        writeHelper(NODE, STATION1.bits.ll3On, true)
+            .then(_ => {
+                return writeHelper(NODE, STATION1.bits.ll3On, false);
+            })
+            .then(_ => {
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
 
     // ll6
     ipcMain.on(CHANNELS.ll3Off, (e, status) => {
-        console.log(status);
-        const NODE = NODE[STATION1.id];
-        NODE.conn.writeItems(STATION1.bits.ll3Off, true, NODE.valuesWritten);
-        setTimeout(() => {
-            NODE.conn.writeItems(STATION1.bits.ll3Off, false, NODE.valuesWritten);
-        }, 1000);
-    });
-
-    // Save
-    ipcMain.on(CHANNELS.step1save, (e, channel, value) => {
-        console.log(channel, value);
-        const step1 = STATION1.datetime.step1;
-
-        if (channel === 'step1OnHH') {
-            writeHelper(NODE, step1.onHHTimer, true)
-                .then(_ => {
-                    return writeHelper(NODE, step1.onHH, value);
-                })
-                .then(_ => {
-                    return writeHelper(NODE, step1.onHHTimer, false);
-                })
-                .then(_ => {
-                    return readHelper(NODE);
-                })
-                .then(data => {
-                    console.log(data[step1.onHH]);
-                });
-        }
+        writeHelper(NODE, STATION1.bits.ll3Off, true)
+            .then(_ => {
+                return writeHelper(NODE, STATION1.bits.ll3Off, false);
+            })
+            .then(_ => {
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
  
     // Set automanual
