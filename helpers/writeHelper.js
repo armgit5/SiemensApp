0dac685a95ba94aa9465c8e517a1ef36fb08a567
Arm Suwansiri <ysuwansiri@yahoo.com>
2020-02-02 22:04:58 +0700

    added support for 3 tries after failing

--- helpers/writeHelper.js
+++ helpers/writeHelper.js
@@ -7,15 +7,31 @@ module.exports = (NODE, register, value) => {
                     console.log("CANNOT WRITE!!!!"); 
                     
                     // Try once again.
-                    NODE.conn.writeItems(register, value, (anythingBad) => { // // Set ll1 M150 true
-                        NODE.doneWriting = true;
-                        if (anythingBad) { 
-                            console.log("CANNOT WRITE 2ND TIME!!!!"); 
-                            reject("CANNOT WRITE 2ND TIME!!!!");
-                        } else {
-                            resolve(register);
-                        }
-                    });
+                    setTimeout(() => {
+                        NODE.conn.writeItems(register, value, (anythingBad) => { // // Set ll1 M150 true
+                            NODE.doneWriting = true;
+                            if (anythingBad) { 
+                                console.log("CANNOT WRITE 2ND TIME!!!!"); 
+                                
+                                 // Try once again.
+                                setTimeout(() => {
+                                    NODE.conn.writeItems(register, value, (anythingBad) => { // // Set ll1 M150 true
+                                        NODE.doneWriting = true;
+                                        if (anythingBad) { 
+                                            console.log("CANNOT WRITE 3RD TIME!!!!"); 
+                                            reject("CANNOT WRITE 3RD TIME!!!!");
+                                        } else {
+                                            resolve(register);
+                                        }
+                                    });
+                                }, 1000);
+
+                            } else {
+                                resolve(register);
+                            }
+                        });
+                    }, 1000);
+
                     
                 } else {
                     resolve(register);
